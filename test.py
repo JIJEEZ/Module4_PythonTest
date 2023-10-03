9cc2c723117cc422ffbcfff7cb8917e63f16152c
NetAcad DEVASC <changeme@example.com>
2023-10-03 08:04:01 +0000

    Testing

---
+++ test.py
@@ -0,0 +1,25 @@
+import math
+
+def test_sqrt():
+    num=25
+    assert math.sqrt(num) == 5
+
+def test_square():
+
+    num = 7
+    assert num*num == 49
+
+def test_check():
+    x = 100
+
+    assert x >= 100
+
+def test_fun():
+    x = 5
+
+    assert x == 5
+
+def test_multiply():
+    x = 4
+    y = 5
+    assert x*y == 20
